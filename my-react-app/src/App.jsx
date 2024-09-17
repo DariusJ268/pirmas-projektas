6f294d2baf04628c25dff70984b7fa8f38f186bd
DariusJ268 <darius.juralevic@gmail.com>
2024-09-17 21:11:40 +0300

    understanding the code

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './components/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Hero from "./components/home/Hero"
+import CategoryList from "./components/home/CategoryList"
 import Topbar from "./components/layout/Topbar";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <>
       <div>
         <Topbar />
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Hero />
+        <CategoryList />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- my-react-app/src/components/layout/Topbar.jsx
+++ my-react-app/src/components/layout/Topbar.jsx
@@ -20,7 +20,7 @@ const Topbar = () => {
     return (
         <div className={styles.topbar}>
             <div className={styles.leftside}>
-            <LuBoxes />
+            <LuBoxes className="topbarIcon" />
                 <nav className={styles.navigation}>
                 {links.map((link) => (
                     <a key={link.label} href={link.href} className={styles.link}>
