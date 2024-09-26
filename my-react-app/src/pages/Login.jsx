968ac089024427ed834fa444898db1f13efb29ae
DariusJ268 <darius.juralevic@gmail.com>
2024-09-26 21:12:59 +0300

    adding login option

--- my-react-app/src/pages/Login.jsx
+++ my-react-app/src/pages/Login.jsx
@@ -1,3 +1,7 @@
+import Input from "@/components/common/Input";
+import styles from "./Login.module.scss";
+import Button from "@/components/common/Button";
+
 const Login = () => {
     return <h1>Login</h1>;
   };
